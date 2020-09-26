595d76ec1fee23b0b639f16b822cfecf5448fc0c
rbwilson-maker <71906807+rbwilson-maker@users.noreply.github.com>
2020-09-25 21:02:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+#from https://www.cs.cmu.edu/~112/notes/notes-graphics.html#customColors
+def rgbString(r, g, b):
+    # Don't worry about the :02x part, but for the curious,
+    # it says to use hex (base 16) with two digits.
+    return f'#{r:02x}{g:02x}{b:02x}'
+
 def drawHangman(canvas, w, h):
     #variables#
     margin = 50
@@ -10,16 +16,19 @@ def drawHangman(canvas, w, h):
     headRadius = h // 10
     bodyLength = 2.5 * headRadius
     armWidth = 3.5 * headRadius
-    
+    darkRed = rgbString(200, 10, 75)
+
+    canvas.create_text(w/2, h/2, text="CMU", font="Roboto 215 bold", fill = darkRed)
+
     #to make post/pole#
     #vertical pole#
-    canvas.create_line(poleX, margin, poleX, margin + poleLength)
+    canvas.create_line(poleX, margin, poleX, margin + poleLength, width=10)
     #pole base#
-    canvas.create_line(margin, h - margin, margin + poleBaseWidth, h - margin)
+    canvas.create_line(margin, h - margin, margin + poleBaseWidth, h - margin, width = 10)
     #top of pole#
-    canvas.create_line(margin + poleTopWidth, margin, margin + poleTopWidth, margin + poleBitLen)
+    canvas.create_line(margin + poleTopWidth, margin, margin + poleTopWidth, margin + poleBitLen, width =10)
     #extra pole bit on top
-    canvas.create_line(margin + poleBaseWidth // 2, margin, margin + poleTopWidth, margin)
+    canvas.create_line(margin + poleBaseWidth // 2, margin, margin + poleTopWidth, margin, width = 10)
     
     #gets cases#
     population = 3000
@@ -30,20 +39,20 @@ def drawHangman(canvas, w, h):
     #makes body based on cases#
     if (caseRatio > increment):
         #head#
-        canvas.create_oval(margin + poleTopWidth - headRadius, margin + poleBitLen, margin + poleTopWidth + headRadius, margin + poleBitLen + 2 * headRadius)
+        canvas.create_oval(margin + poleTopWidth - headRadius, margin + poleBitLen, margin + poleTopWidth + headRadius, margin + poleBitLen + 2 * headRadius, width = 10)
         if (caseRatio > 2*increment):
             #body#
-            canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2, margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength)
+            canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2, margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, width = 10)
             if (caseRatio > 3*increment):
                 #arms#
-                canvas.create_line(margin + poleTopWidth - armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3, margin + poleTopWidth + armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3)
+                canvas.create_line(margin + poleTopWidth - armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3, margin + poleTopWidth + armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3, width = 10)
                 if (caseRatio > 4*increment):
                     #left leg#
-                    canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, margin + poleTopWidth - armWidth // 2, h - margin * 2)
+                    canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, margin + poleTopWidth - armWidth // 2, h - margin * 2, width = 10)
                     if(caseRatio> 5*increment):
                         #right leg#
-                        canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, margin + poleTopWidth + armWidth // 2, h - margin * 2)
-                        canvas.create_text(w/2, h/2, text="!!!GO HOME!!!", font = "Nunito 38 bold", color = "red")
+                        canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, margin + poleTopWidth + armWidth // 2, h - margin * 2, width = 10)
+                        canvas.create_text(w/2, h/2, text="!!!GO HOME!!!", font = "Nunito 58 bold", fill = rgbString(240,240,50))
 
 def makeCanvas(w, h):
     root = tk.Tk()
