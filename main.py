26c75c2b3be53f0c02762fa06c7fc419c797ce32
mgthaitang <71906824+mgthaitang@users.noreply.github.com>
2020-09-25 20:21:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 def drawHangman(canvas, w, h):
+    #variables#
     margin = 50
     poleLength = h - 2 * margin
     poleBaseWidth = w // 10
@@ -9,10 +10,12 @@ def drawHangman(canvas, w, h):
     headRadius = h // 10
     bodyLength = 3 * headRadius
     armWidth = 4 * headRadius
+    #to make post/pole#
     canvas.create_line(poleX, margin, poleX, margin + poleLength)
     canvas.create_line(margin, h - margin, margin + poleBaseWidth, h - margin)
     canvas.create_line(margin + poleTopWidth, margin, margin + poleTopWidth, margin + poleBitLen)
     canvas.create_line(margin + poleBaseWidth // 2, margin, margin + poleTopWidth, margin)
+    #to make body#
     canvas.create_oval(margin + poleTopWidth - headRadius, margin + poleBitLen, margin + poleTopWidth + headRadius, margin + poleBitLen + 2 * headRadius, fill = "white")
     canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2, margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength)
     canvas.create_line(margin + poleTopWidth - armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3, margin + poleTopWidth + armWidth // 2, margin + poleBitLen + headRadius * 2 + bodyLength // 3)
@@ -26,3 +29,5 @@ def makeCanvas(w, h):
     root.mainloop()
 
 makeCanvas(500, 500)
+
+x = input("how many cases???")
