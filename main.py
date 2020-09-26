b723411f6777943e6b22f8512aa95dc310a3beaf
dinnerdash6 <71907091+dinnerdash6@users.noreply.github.com>
2020-09-26 13:49:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,8 +95,8 @@ def drawHead(canvas, margin, headRadius, poleBitLen, poleTopWidth):
 def drawMask(canvas, margin, poleTopWidth, headRadius, poleBitLen):
     maskRatio = 0.75
     fill = 'black'
-    canvas.create_rectangle(margin + poleTopWidth - headRadius, margin + poleBitLen + headRadius, margin + poleTopWidth + headRadius, margin + poleBitLen + headRadius + 5, fill = fill)
-    canvas.create_rectangle(15 + margin + poleTopWidth - headRadius, margin + poleBitLen + headRadius, margin + poleTopWidth + headRadius - 15, margin + poleBitLen + headRadius + headRadius * maskRatio, fill = fill)
+        canvas.create_rectangle(margin + poleTopWidth - headRadius, margin + poleBitLen + headRadius, margin + poleTopWidth + headRadius, 1.1*margin + poleBitLen + headRadius, fill = fill)
+    canvas.create_rectangle(1.3* margin + poleTopWidth - headRadius, margin + poleBitLen + headRadius, 0.7*margin + poleTopWidth + headRadius, margin + poleBitLen + headRadius + headRadius * maskRatio, fill = fill)
 
 def drawBody(canvas, margin, poleTopWidth, poleBitLen, headRadius, bodyLength):
     canvas.create_line(margin + poleTopWidth, margin + poleBitLen + headRadius * 2, margin + poleTopWidth, margin + poleBitLen + headRadius * 2 + bodyLength, width = 10)
